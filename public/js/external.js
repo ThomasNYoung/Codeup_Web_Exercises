7f3ffce24702d53e08ce1f85c73f242dd179102f
Tom Young <tom.young.satx@gmail.com>
2015-06-17 13:10:54 -0500

    break_continue_js html

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,16 @@
 console.log('hello from external javascript'); 
+
+if (true){
+	console.log("this should happen");
+}
+
+var someboolean = true;
+
+if (someboolean) {
+	console.log("this variable is true");
+}
+
+var someboolean = false;
+if (someboolean) {
+	console.log("this variable is " + someboolean);
+}
